874866f178d70935ebfa7a370a40106e6c66e0d4
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-23 22:49:48 +0530

    init: hello world

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold font-poppins text-gray-600">Hello world!</h1>
     </div>
   );
 }
