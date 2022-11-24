9d1952e2dc959f86c3aa676db24fc3cae14c4b6d
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-24 12:09:47 +0530

    update:proptype

--- src/components/Layout/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Layout/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function BurgerIngredient(props) {
   let ingredient = null;
@@ -18,15 +19,33 @@ function BurgerIngredient(props) {
       );
       break;
     case "meat":
-        ingredient = (
-            <div className="w-4/5 h-1/6 bg-gradient-to-r from-orange-400 to-orange-700 rounded-t-md shadow-inner mx-2 my-auto"></div>
-        )
-        break;
+      ingredient = (
+        <div className="w-4/5 h-1/6 bg-gradient-to-r from-orange-400 to-orange-700 rounded-t-md shadow-inner mx-2 my-auto"></div>
+      );
+      break;
+    case "cheese":
+      ingredient = (
+        <div className="w-4/5 h-1/6 bg-gradient-to-r from-yellow-400 to-yellow-700 rounded-t-md shadow-inner mx-2 my-auto"></div>
+      );
+      break;
+    case "salad":
+      ingredient = (
+        <div className="w-4/5 h-1/6 bg-gradient-to-r from-green-800 to-green-300 rounded-t-md shadow-inner mx-2 my-auto"></div>
+      );
+      break;
+    case "bacon":
+      ingredient = (
+        <div className="w-4/5 h-1/6 bg-gradient-to-r from-yellow-800 to-yellow-300 rounded-t-md shadow-inner mx-2 my-auto"></div>
+      );
+      break;
     default:
-        ingredient = null;
-        
+      ingredient = null;
   }
-  return <div>BurgerIngredient</div>;
+  return ingredient;
+}
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired,
 }
 
 export default BurgerIngredient;
