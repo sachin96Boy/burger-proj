6de8afb78e160722eef4540a79a0ffa9092c936a
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-24 14:04:05 +0530

    style: build control

---
+++ src/components/Layout/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BuildControls(props) {
+  return (
+    <div className="w-full bg-orange-600 flex flex-grow flex-col items-center shadow-md m-auto px-3">
+      BuildControls
+    </div>
+  );
+}
+
+export default BuildControls;
---
+++ src/components/Layout/Burger/Buildontroller/BuildControlller.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function BuildControlller(props) {
+  return (
+    <div className="flex justify-between items-center mt-1">
+      <div className="p-3 font-bold w-20">{props.label}</div>
+      <button className="disabled:bg-orange-300 disabled:text-white disabled:cursor-not-allowed block p-1 mx-1 w-20 border outline-none cursor-pointer border-orange-500">
+        Less
+      </button>
+      <button className="disabled:bg-orange-300 disabled:text-white disabled:cursor-not-allowed block p-1 mx-1 w-20 border outline-none cursor-pointer border-orange-500">
+        More
+      </button>
+    </div>
+  );
+}
+
+export default BuildControlller;
