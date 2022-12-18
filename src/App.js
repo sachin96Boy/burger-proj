f774f7f2be47b743582acb422cd56bb34a179e0e
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-12-18 14:25:21 +0530

    update: logout Logic

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Checkout from "./containers/checkout/Checkout";
 import { Route, Routes } from "react-router-dom";
 import Orders from "./containers/orders/Orders";
 import Auth from "./containers/Auth/Auth";
+import Logout from "./containers/Auth/logout/Logout";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="checkout/*" element={<Checkout />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/logout" element={<Logout />} />
           <Route path="/" exact element={<BurgerBuilder />} />
         </Routes>
       </Layout>
