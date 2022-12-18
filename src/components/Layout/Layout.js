8062ea25fe95477f0625ee73de49a771d2c38d17
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-12-18 14:06:34 +0530

    update: handle logout on UI

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,16 +2,22 @@ import React from "react";
 import Auxilary from "../../hoc/Auxilary";
 import SideDrawer from "../Navigation/NavigationItems/SideDrawer/SideDrawer";
 import TopBar from "../Navigation/Toolbar/TopBar";
+import { useSelector } from "react-redux";
 
 function Layout(props) {
   const [showSideDrawer, setShowSideDrawer] = React.useState(false);
   const sideDrawerCloseHandler = () => {
     setShowSideDrawer(!showSideDrawer);
   }
+  const selector = useSelector((state) => {
+    return {
+      isAuthenticated: state.auth.token !== null,
+    };
+  });
   return (
     <Auxilary>
-      <TopBar sideDrawerHandler={sideDrawerCloseHandler} />
-      <SideDrawer open={showSideDrawer} closed={sideDrawerCloseHandler}/>
+      <TopBar sideDrawerHandler={sideDrawerCloseHandler} isAuthenticated={selector.isAuthenticated}/>
+      <SideDrawer open={showSideDrawer} closed={sideDrawerCloseHandler} isAuthenticated={selector.isAuthenticated}/>
       <main className="mt-20">{props.children}</main>
     </Auxilary>
   );
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import NavigationItem from './NavigationItem/NavigationItem'
+import React from "react";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
 export default function NavigationItems(props) {
   return (
-    <ul className='m-0 p-0 flex flex-col items-center h-full lg:flex-row'>
-        <NavigationItem link="/" exact>Burger builder</NavigationItem>
-        <NavigationItem link="/orders">Orders</NavigationItem>
+    <ul className="m-0 p-0 flex flex-col items-center h-full lg:flex-row">
+      <NavigationItem link="/" exact>
+        Burger builder
+      </NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
+      {!props.isAuthenticated ? (
         <NavigationItem link="/auth">Auth</NavigationItem>
+      ) : (
+        <NavigationItem link="/logout">Logout</NavigationItem>
+      )}
     </ul>
-  )
+  );
 }
